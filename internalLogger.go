3148eaea4a370a4ee7a34616434dbd07faa31a34
umenosuke <umenosuke.net@gmail.com>
2024-10-19 13:33:44 +0900

    internalLoggerはsync.Mutexではなくした 内部利用のみなので設定変更などがない

--- internalLogger.go
+++ internalLogger.go
@@ -4,20 +4,16 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"sync"
 )
 
 // Use for logger's own log output.
 type tInternalLogger struct {
-	sync.Mutex
 	writer io.Writer
 	prefix string
 	flg    LogLevel
 }
 
 func (thisLogger *tInternalLogger) log(timestamp string, fileName string, msg string) {
-	thisLogger.Lock()
-	defer thisLogger.Unlock()
 	fmt.Fprintln(thisLogger.writer, timestamp+thisLogger.prefix+" "+fileName+": "+msg)
 }
 
